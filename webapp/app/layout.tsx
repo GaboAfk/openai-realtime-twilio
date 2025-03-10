485a5d643abd2b5103c93028c82fda20fb109c60
GaboAfk <gabrielmorffe95@gmail.com>
2025-03-10 20:25:12 -0300

    willinn colors + favicon

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "OpenAI Realtime + Twilio",
+  title: "Realtime + Twilio",
   description:
     "Sample phone call assistant app for OpenAI Realtime API and Twilio",
 };
